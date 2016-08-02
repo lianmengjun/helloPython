d09dcfdcd6e6a61c7fecb2b0c38705831321fdfa
lianmengjun <lianmengjun@gmail.com>
2016-08-02 23:45:58 +0800

    hello python

---
+++ src/insertionSort.py
@@ -0,0 +1,25 @@
+'''
+Created on 2016Äê8ÔÂ2ÈÕ
+
+@author: Administrator
+'''
+
+def insert_sort(lists):
+    count = len(lists)
+    for i in range(1,count):
+        key=lists[i]
+        j=i-1
+        while j>=0:
+            if lists[j]>key:
+                lists[j+1]=lists[j]
+                lists[j] =key
+                j -=1
+    return lists
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
